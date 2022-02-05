bbf9abca23b370d8f322bc6a5bd138118c02ff66
Huanderson Lima <mattewlima16@gmail.com>
2022-02-04 22:50:51 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using dotnet_poo_rpg.src.Entities;
+
+namespace dotnet_poo_rpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight Arus = new Knight("Arus",2, "Knight");
+            Wizard Jennica = new Wizard("Jennica",23, "White Wizard");
+
+            //Console.WriteLine(Arus.ToString());
+            
+            Console.WriteLine(Jennica.Attack(4));
+            Console.WriteLine(Jennica.Attack(8));
+            
+        }
+    }
+}
